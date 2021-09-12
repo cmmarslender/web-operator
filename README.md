52ac13901c0a61375e7b56ad8c9ab3cb19a65679
Chris Marslender <chrismarslender@gmail.com>
2021-09-11 22:07:00 -0700

    Add a basic managed ConfigMap as a test / to get everything working

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Generated using kubebuilder.
 Project initialized with `kubebuilder init --domain k8s.cmm.io --repo github.com/cmmarslender/web-operator`
 
 Additional APIs created with commands like `kubebuilder create api --group webapp --version v1 --kind SimpleApp`
+
+Resource reconciliation utilizes `github.com/banzaicloud/operator-tools/pkg/reconciler` as a reconciler.
--- controllers/simpleapp_controller.go
+++ controllers/simpleapp_controller.go
@@ -18,18 +18,22 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/banzaicloud/operator-tools/pkg/reconciler"
+	webappv1 "github.com/cmmarslender/web-operator/api/v1"
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	webappv1 "github.com/cmmarslender/web-operator/api/v1"
 )
 
 // SimpleAppReconciler reconciles a SimpleApp object
 type SimpleAppReconciler struct {
 	client.Client
+	Log    logr.Logger
 	Scheme *runtime.Scheme
 }
 
@@ -47,16 +51,48 @@ type SimpleAppReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *SimpleAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	r.Log.Info(fmt.Sprintf("SimpleApp name is %s", req.NamespacedName))
 
-	// your logic here
+	resourceReconciler := reconciler.NewReconcilerWith(r.Client, reconciler.WithLog(r.Log))
+
+	var app webappv1.SimpleApp
+	if err := r.Get(ctx, req.NamespacedName, &app); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Create a dummy config map with the value from the CRD, just as a test
+	configMapObject := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-configmap",
+			Namespace: req.Namespace,
+		},
+		Data: map[string]string{
+			"foo": app.Spec.Foo,
+		},
+	}
+
+	err := ctrl.SetControllerReference(&app, configMapObject, r.Scheme)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	result, err := resourceReconciler.ReconcileResource(configMapObject, reconciler.StatePresent)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if result != nil {
+		return *result, nil
+	}
 
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *SimpleAppReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	r.Log = ctrl.Log.WithName("controllers").WithName("SimpleApp")
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&webappv1.SimpleApp{}).
+		Owns(&corev1.ConfigMap{}).
 		Complete(r)
 }
